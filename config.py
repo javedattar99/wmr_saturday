85f3f85cad54485935a20298cd2cebaf5a1fe28c
javedattar99 <62836302+javedattar99@users.noreply.github.com>
2023-07-08 18:09:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 SECRET_KEY = 'django-insecure-$04zfwxft@y#2c14l_@3eebwta_es1o7odi^b!9!0hxcsocg%v'
 DB_ENGINE = 'django.db.backends.postgresql'
 NAME = 'WMR_DB_2023'
-DB_USER = 'postgres'
-DB_PASSWORD= 'Miba#12345'
+DB_USER = 'wmr'
+DB_PASSWORD= 'wmr123'
 DB_HOST= 'localhost'
 PORT= ''
 
